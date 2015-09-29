3ad221778024cef588e6b43434b4d43462143784
Junaide Paul <junaidpaul90@gmail.com>
2015-09-28 23:25:37 -0400

    fiser yates algorithm

---
+++ shuffle/fisher-yates-shuffle.js
@@ -0,0 +1,22 @@
+var shuffle = function(array) {
+  var copy = [], n = array.length, i;
+
+  // While there remain elements to shuffle…
+  while (n) {
+
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * array.length);
+
+    // If not already shuffled, move it to the new array.
+    if (i in array) {
+      copy.push(array[i]);
+      delete array[i];
+      n--;
+    }
+  }
+
+  return copy;
+}
+var arr = [2,6,4,1,9,8,7];
+var ans = shuffle(arr);
+console.log(ans);
